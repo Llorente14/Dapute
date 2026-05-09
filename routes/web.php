d3a517927147a918dd30d19c74df7b23ec823aaf
Mikeee636 <michael.535240080@stu.untar.ac.id>
2026-05-09 23:28:58 +0700

    feat: [SCRUM-27] membuat UI login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Auth\LoginForm;
+use App\Livewire\Auth\RegisterForm;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', LoginForm::class)->middleware('guest')->name('login');
+Route::get('/register', RegisterForm::class)->middleware('guest')->name('register');
